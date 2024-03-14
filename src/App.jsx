41cf567c408139acd7a7abf98f34f61b19d7549a
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2024-03-14 14:11:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1 className='text-4xl bg-red-500'>Checking All UI</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <div className='container mx-auto h-28 bg-blue-400'>
+        <h1>I am tomato</h1>
+      </div>
+      <button className="btn btn-primary">Primary</button>
+
+      <div className='space-x-5'>
+        <button className="btn btn-info">Info</button>
+        <button className="btn btn-success">Success</button>
+        <button className="btn btn-warning">Warning</button>
+        <button className="btn btn-error">Error</button>
+      </div>
+
+    </>
+  )
+}
+
+
+
+export default App
